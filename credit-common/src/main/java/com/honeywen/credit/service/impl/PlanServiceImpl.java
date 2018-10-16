45f19111b9c0d49e8a1d004bff648f0d9ac8ac8a
weiw <weiw@opera.com>
2018-10-16 10:47:56 +0800

    plan

--- credit-common/src/main/java/com/honeywen/credit/service/impl/PlanServiceImpl.java
+++ credit-common/src/main/java/com/honeywen/credit/service/impl/PlanServiceImpl.java
@@ -4,12 +4,10 @@ import com.honeywen.credit.dto.EventDTO;
 import com.honeywen.credit.model.Card;
 import com.honeywen.credit.repository.query.CardQueryMapper;
 import com.honeywen.credit.service.PlanService;
-import org.apache.poi.ss.formula.functions.Even;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.EventListener;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,10 +31,9 @@ public class PlanServiceImpl implements PlanService {
 
         return cards.stream().map(card -> {
 
-            int billDay = card.getBillDay();
             return EventDTO.builder()
                     .title("刷" + card.getName())
-                    .start(now.withDayOfMonth(billDay + 1).toString())
+                    .start(now.withDayOfMonth(card.getBillDay() + 1).toString())
                     .build();
 
         }).collect(Collectors.toList());
