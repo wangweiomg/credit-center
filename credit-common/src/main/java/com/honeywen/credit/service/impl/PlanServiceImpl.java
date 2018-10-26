b06db34cae8c6902b6d445cfb88ee4fcfc857db3
weiw <weiw@opera.com>
2018-10-26 13:27:56 +0800

    change style

--- credit-common/src/main/java/com/honeywen/credit/service/impl/PlanServiceImpl.java
+++ credit-common/src/main/java/com/honeywen/credit/service/impl/PlanServiceImpl.java
@@ -1,12 +1,15 @@
 package com.honeywen.credit.service.impl;
 
+import com.google.common.collect.Lists;
 import com.honeywen.credit.dto.EventDTO;
 import com.honeywen.credit.model.Card;
 import com.honeywen.credit.repository.query.CardQueryMapper;
 import com.honeywen.credit.service.PlanService;
+import org.apache.poi.ss.formula.functions.Even;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,15 +32,33 @@ public class PlanServiceImpl implements PlanService {
 
         final LocalDate now = LocalDate.now();
 
-        return cards.stream().map(card -> {
+        List<EventDTO> list = Lists.newArrayList();
 
-            return EventDTO.builder()
-                    .title("刷" + card.getName())
-                    .start(now.withDayOfMonth(card.getBillDay() + 1).toString())
+        //
+        cards.forEach(i -> {
+
+            // 哪一天，刷多少
+            int billDay = i.getBillDay();
+            EventDTO e1 = EventDTO.builder()
+                    .start(now.withDayOfMonth(billDay + 1).toString())
+                    .title("刷" + i.getName() + ", 金额：￥" + i.getCardLimit().divide(BigDecimal.valueOf(4L), BigDecimal.ROUND_FLOOR))
                     .build();
+            EventDTO e2 = EventDTO.builder()
+                    .start(now.withDayOfMonth(billDay + 2).toString())
+                    .title("刷" + i.getName() + ", 金额：￥" + i.getCardLimit().divide(BigDecimal.valueOf(4L), BigDecimal.ROUND_FLOOR))
+                    .build();
+            EventDTO e3 = EventDTO.builder()
+                    .start(now.withDayOfMonth(billDay + 3).toString())
+                    .title("刷" + i.getName() + ", 金额：￥" + i.getCardLimit().divide(BigDecimal.valueOf(4L), BigDecimal.ROUND_FLOOR))
+                    .build();
+            list.add(e1);
+            list.add(e2);
+            list.add(e3);
 
-        }).collect(Collectors.toList());
 
+        });
+
+        return list;
 
     }
 }
