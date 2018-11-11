193670fc1996f42db75be296ebc5f8417e3a647e
weiw <weiw@opera.com>
2018-11-11 22:27:35 +0800

    add msg token

--- credit-admin/src/main/java/com/honeywen/credit/controller/WxPortalController.java
+++ credit-admin/src/main/java/com/honeywen/credit/controller/WxPortalController.java
@@ -24,7 +24,7 @@ public class WxPortalController {
                           @RequestParam(name = "timestamp", required = false) String timestamp,
                           @RequestParam(name = "nonce", required = false) String nonce,
                           @RequestParam(name = "echostr", required = false) String echostr) {
-        log.info("\n接收到来自微信服务器的认证消息：signature = [{}], timestamp = [{}], nonce = [{}], echostr = [{}]",
+        log.debug("\n接收到来自微信服务器的认证消息：signature = [{}], timestamp = [{}], nonce = [{}], echostr = [{}]",
                 signature, timestamp, nonce, echostr);
 
         if (StringUtils.isAnyBlank(signature, timestamp, nonce, echostr)) {
