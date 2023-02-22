4aed85dac1edde566b3728040d5676d5adedd627
welton <wangwei_omg@163.com>
2023-02-22 10:37:13 +0800

    add deploy shells

---
+++ scripts/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CODE_PATH='/home/jack/git/credit-center'
+TOMCAT_PATH='/home/jack/tomcat-credit'
+# 1. pull code
+cd $CODE_PATH
+git pull
+git reset --hard HEAD
+# 2. package war
+cd $CODE_PATH
+mvn clean package -DskipTests
+# 3. stop tomcat and clear ROOT
+bash $TOMCAT_PATH/bin/shutdown.sh
+sleep 2
+#rm -rf $TOMCAT_PATH/webapps/ROOT/*
+
+# 4. mv war to tomcat ROOT and unzip
+unzip -o $CODE_PATH/credit-webapp/target/credit.war -d $TOMCAT_PATH/webapps/ROOT/
+
+# 5. start tomcat
+bash $TOMCAT_PATH/bin/startup.sh
+
+# 6. watch
+tail -f -n100 $TOMCAT_PATH/logs/catalina.out
+
+
+
