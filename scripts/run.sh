3aa779f7274d8d28d074d5faff703fd3828b20d7
welton <wangwei_omg@163.com>
2023-02-23 15:44:40 +0800

    update deploy shell

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,26 +2,77 @@
 
 CODE_PATH='/home/jack/git/credit-center'
 TOMCAT_PATH='/home/jack/tomcat-credit'
+
 # 1. pull code
-cd $CODE_PATH
-git pull
-git reset --hard origin/master
+function updateCode(){
+    cd $CODE_PATH
+    echo " "
+    echo -e "\033[35m start update project source code \033[0m"
+    git pull
+    git reset --hard origin/master
+
+    echo -e "\033[35m end update project source code \033[0m"
+
+}
+
+
 # 2. package war
-cd $CODE_PATH
-mvn clean package -DskipTests
+function package(){
+
+    cd $CODE_PATH
+    echo -e "\033[35m start package project \033[0m"
+    mvn clean package -U -DskipTests
+    echo -e "\033[35m end package project \033[0m"
+}
+
+
 # 3. stop tomcat and clear ROOT
-bash $TOMCAT_PATH/bin/shutdown.sh
-sleep 2
+function stopServer(){
+    bash $TOMCAT_PATH/bin/shutdown.sh
+    sleep 5
+
+}
 #rm -rf $TOMCAT_PATH/webapps/ROOT/*
 
 # 4. mv war to tomcat ROOT and unzip
-unzip -o $CODE_PATH/credit-webapp/target/credit.war -d $TOMCAT_PATH/webapps/ROOT/
+function copyFiles(){
+    unzip -o $CODE_PATH/credit-webapp/target/credit.war -d $TOMCAT_PATH/webapps/ROOT/
+
+}
 
 # 5. start tomcat
-bash $TOMCAT_PATH/bin/startup.sh
+function run(){
+
+    bash $TOMCAT_PATH/bin/startup.sh
+}
 
 # 6. watch
-tail -f -n100 $TOMCAT_PATH/logs/catalina.out
+function watch(){
+    tail -f -n100 $TOMCAT_PATH/logs/catalina.out |
+        while IFS= read line
+            do
+                echo "$line"
+                if [[ "$line" == *"Server startup in"* ]];then
+                    echo -e "\033[35m success $line \033[0m"
+                    pkill tail
+                fi
+        done
+
+}
 
 
+
+### function calls
+
+updateCode
+
+package
+
+stopServer
+
+copyFiles
+
+run
+
+watch
 
